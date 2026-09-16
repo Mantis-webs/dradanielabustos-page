92f841f01c3329112d67665dd440c169ced5c480
zabroso <pjmarambioc@gmail.com>
2026-09-16 15:19:36 -0300

    feat(product): embebe formulario de agendamiento de Go High Level
    Agrega el componente Formulario con el iframe embed del form de GHL
    y lo monta en App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import Formulario from './components/Formulario'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -114,6 +115,7 @@ function App() {
       </section>
 
       <div className="ticks"></div>
+      <Formulario/>
       <section id="spacer"></section>
     </>
   )
---
+++ src/components/Formulario.tsx
@@ -0,0 +1,43 @@
+function Formulario() {
+  return (
+    <section
+      id="form-container"
+      style={{
+        width: "100%",
+        maxWidth: "900px",
+        margin: "0 auto",
+        padding: "40px 20px",
+        boxSizing: "border-box",
+      }}
+    >
+      <iframe
+        src="https://api.leadconnectorhq.com/widget/form/i5J4FVtDg6IGXxDinIBS"
+        style={{
+          width: "100%",
+          minHeight: "745px",
+          height: "745px",
+          border: "none",
+          borderRadius: "8px",
+          display: "block",
+        }}
+        id="inline-i5J4FVtDg6IGXxDinIBS"
+        data-layout='{"id":"INLINE"}'
+        data-trigger-type="alwaysShow"
+        data-trigger-value=""
+        data-activation-type="alwaysActivated"
+        data-activation-value=""
+        data-deactivation-type="neverDeactivate"
+        data-deactivation-value=""
+        data-form-name="Formulario - Dra. Daniela Bustos - Page"
+        data-height="745"
+        data-layout-iframe-id="inline-i5J4FVtDg6IGXxDinIBS"
+        data-form-id="i5J4FVtDg6IGXxDinIBS"
+        data-cookie-consent="true"
+        data-cookie-consent-provider="auto"
+        title="Formulario - Dra. Daniela Bustos - Page"
+      />
+    </section>
+  );
+}
+
+export default Formulario;
